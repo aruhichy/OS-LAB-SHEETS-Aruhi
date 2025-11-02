fcf6cf2454dde87fc4e54bb34b6962f444ba1005
aruhichy <aruhichoudhary795@gmail.com>
2025-11-02 11:54:06 +0530

    TASK 2
    Added a dummy function to simulate an OS process.

---
+++ LAB SHEET 2/solutions/task2.c
@@ -0,0 +1,7 @@
+# Sub-Task 2: Simulate a system process
+# Objective: Create a dummy function to mimic an OS process
+
+import time
+import loggingsss
+# Test call to function
+system_process("Demo Process")
