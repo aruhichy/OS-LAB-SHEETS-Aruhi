fcda398446f9e68626c4566b9c33ed0632a2a5a9
aruhichy <aruhichoudhary795@gmail.com>
2025-11-19 12:22:57 +0530

    TASK 1
    Added Round Robin scheduling implementation to task1.c.

--- LAB SHEET 3/task1.c
+++ LAB SHEET 3/task1.c
@@ -1,3 +1,4 @@
+# Priority Scheduling
 processes = []
 n = int(input("Enter number of processes: "))
 for i in range(n):
@@ -19,3 +20,43 @@ for pid, bt, pr in processes:
 print(f"Average Waiting Time: {total_wt / n}")
 print(f"Average Turnaround Time: {total_tt / n}")
 
+# ROUND ROBIN SCHEDULING
+CODE-
+print("\n----- ROUND ROBIN SCHEDULING -----")
+n2 = int(input("Enter number of processes: "))
+burst = []
+for i in range(n2):
+    bt = int(input("Enter Burst Time for P" + str(i+1) + ": "))
+    burst.append(bt)
+quantum = int(input("Enter Time Quantum: "))
+rem = burst[:]       # remaining times
+wt = [0] * n2        # waiting times
+tat = [0] * n2       # turnaround times
+t = 0                # current time
+
+while True:
+    done = True
+    for i in range(n2):
+        if rem[i] > 0:
+            done = False
+            if rem[i] > quantum:
+                t += quantum
+                rem[i] -= quantum
+            else:
+                t += rem[i]
+                wt[i] = t - burst[i]
+                rem[i] = 0
+
+    if done:
+        break
+
+for i in range(n2):
+    tat[i] = burst[i] + wt[i]
+
+print("\nPID\tBT\tWT\tTAT")
+
+for i in range(n2):
+    print("P" + str(i+1) + "\t" + str(burst[i]) + "\t" + str(wt[i]) + "\t" + str(tat[i]))
+
+print("\nAverage Waiting Time:", sum(wt)/n2)
+print("Average Turnaround Time:", sum(tat)/n2)
